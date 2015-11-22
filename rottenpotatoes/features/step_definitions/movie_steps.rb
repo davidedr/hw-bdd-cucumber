74401e3e64939d715a35929e11ccb108e7c7c91e
davidedr <d.dr@libero.it>
2015-11-22 18:57:28 +0000

    Scenario: 'restrict to movies with 'PG' or 'R' ratings' added

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -17,6 +17,19 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
   #fail "Unimplemented"
+  
+  i1 = page.text.index(e1)
+  i2 = page.text.index(e2)
+  if not i1
+    assert false
+  end
+  if not i2
+    assert false
+  end
+  if not (i1 < i2)
+    assert false
+  end  
+  
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -27,10 +40,26 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  #fail "Unimplemented"
+  
+  rating_list.split(",") do |rating|
+    if uncheck
+      When %Q{"I uncheck"ratings[#{rating}]}
+    else
+      When %Q{"I check"ratings[#{rating}]}
+    end
+  end
+  
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  #fail "Unimplemented"
+  
+  Movie.all.each do |movie|
+    if not page.text.index(movie.title)
+      assert false
+    end
+  end
+  
 end
